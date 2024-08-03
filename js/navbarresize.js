048aa99cb713b8a2a65ac219687f46ecb33553ba
tubaplayerdis <70219635+tubaplayerdis@users.noreply.github.com>
2024-08-03 14:20:00 +0100

    full impl of desktop and mobile navbar resizing

--- js/navbarresize.js
+++ js/navbarresize.js
@@ -1,3 +1,14 @@
+window.onload = function() {
+    if(window.innerWidth < 600) {
+        document.getElementById("navbardesktop").style.setProperty("display", "none", "")
+        document.getElementById("navbarmobile").style.setProperty("display", "flex", "")
+    } else {
+        document.getElementById("navbarmobile").style.setProperty("display", "none", "")
+        document.getElementById("navbardesktop").style.setProperty("display", "flex", "")
+    }
+    console.log("Set navbar onload")
+}
+
 window.addEventListener('resize', function(event){
     if(window.innerWidth < 600) {
         document.getElementById("navbardesktop").style.setProperty("display", "none", "")
@@ -6,4 +17,5 @@ window.addEventListener('resize', function(event){
         document.getElementById("navbarmobile").style.setProperty("display", "none", "")
         document.getElementById("navbardesktop").style.setProperty("display", "flex", "")
     }
+    console.log("Set navbar onresize")
 });
