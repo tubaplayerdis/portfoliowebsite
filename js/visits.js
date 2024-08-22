9fdb909dc4c589fb35536587e9e7ab7450c9a1ec
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2024-08-22 13:27:52 -0500

    Update visits.js

--- js/visits.js
+++ js/visits.js
@@ -1,12 +1,6 @@
-window.onload = function() {
-  var obj
-  
+window.onload = function() {  
   fetch("http://aaronwilk.dev/visit")
   .then(response => response.text())
-  .then(text => console.log(text))
-  .then(data => {
-    obj = data;
-   })
+  .then(text => console.log(`Number of visits to the site: ${text}`))
 
-  console.log("Number of visits to the site: "+obj.visits)
 }
