908995abe36b582560ed546519e95f7572237ff3
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2024-08-22 13:36:10 -0500

    Update visits.js

--- js/visits.js
+++ js/visits.js
@@ -5,7 +5,8 @@ window.onload = function() {
   	return response.json();
   }).then(function (data) {
   	// This is the JSON from our response
-  	console.log(data);
+    console.log("total visits")
+  	console.log(data.visits);
   }).catch(function (err) {
   	// There was an error
   	console.warn('Something went wrong.', err);
