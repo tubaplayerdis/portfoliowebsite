2a4ef7c8d43cb56f86cd7064b1dd8bf42b400490
Tuba Player <70219635+tubaplayerdis@users.noreply.github.com>
2024-08-22 14:29:51 -0500

    Update visits.js

--- js/visits.js
+++ js/visits.js
@@ -5,7 +5,20 @@ window.onload = function() {
   	return response.json();
   }).then(function (data) {
   	// This is the JSON from our response
-    console.log("total visits")
+    console.log("total visits as post")
+  	console.log(data.visits);
+  }).catch(function (err) {
+  	// There was an error
+  	console.warn('Something went wrong.', err);
+  });
+
+  fetch("https://aaronwilk.dev/visit", {method: "GET"})
+    .then(function (response) {
+  	// The API call was successful!
+  	return response.json();
+  }).then(function (data) {
+  	// This is the JSON from our response
+    console.log("total visits as get")
   	console.log(data.visits);
   }).catch(function (err) {
   	// There was an error
